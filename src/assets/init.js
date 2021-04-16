b68e1fa3f245cf16544f289d4bc5d9e11e06e51b
JustisBin <dmlqls2822@naver.com>
2021-04-16 19:02:27 +0900

    layout update

---
+++ src/assets/init.js
@@ -0,0 +1,8 @@
+(function ($) {
+  console.log('>')
+  $(function () {
+    console.log('0000')
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
